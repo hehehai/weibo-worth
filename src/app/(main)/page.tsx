c000eb4a1a80318c7f84416f2eb2892f97b11322
hehehai <riverhohai@gmail.com>
2024-01-20 18:17:50 +0800

    fix: search params

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -19,7 +19,7 @@ export default function Home({
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
   const query = SearchParamsSchema.parse(
-    searchParams?.length ? searchParams : getRandomUser()
+    Object.keys(searchParams ?? {}).length ? searchParams : getRandomUser()
   );
 
   return (
